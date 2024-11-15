24a5dbe5d9152532cd6178101b32c0150a92c875
Ekaterina363 <batsl363@gmail.com>
2024-11-15 18:28:44 +0400

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+my_string =  input("You name  " )
+print("Hallo,", my_string, "You name?".upper())
+print("Hallo,", my_string, "You name?".lower())
+print("Hallo,", my_string, "You name?" .replace(" ","" ))
+print(my_string[0])
+print(my_string[-1]) 
+
